f1bfac0d024f03a79c861893edbb4d40b1e21a5f
xxxANTxxx <86019515+xxxANTxxx@users.noreply.github.com>
2022-05-22 22:58:15 +0300

    Start Startup-Summer-2022

---
+++ src/components/Repositorie/Repositorie.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './Repositorie.css';
+
+const Repositorie = ({ html_url, name, description }) => {
+  return (
+    <div className="repositorie">
+      <h3 className="repositorie__title">
+        <a href={html_url} target="about_blank">{name}</a>
+      </h3>
+      <div className="repositorie__subtitle">{description}</div>
+    </div>
+  )
+}
+
+export default Repositorie
